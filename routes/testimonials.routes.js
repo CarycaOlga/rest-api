f7554829ed554aa9a53185a39a1fec990eeda688
Natalia Lemańczyk <nataliaolga2@gmail.com>
2021-04-23 19:50:47 +0200

    fix bugs

--- routes/testimonials.routes.js
+++ routes/testimonials.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const db = require('./../db');
+const db = require('../db');
 const { v4: uuidv4 } = require('uuid');
 
 const okMessage = {message: 'OK!'};
@@ -8,14 +8,13 @@ const okMessage = {message: 'OK!'};
 router.route('/testimonials').get ((req, res) => {
   res.json(db.testimonials);
 });
-  
+router.route('/testimonials/random').get ((req, res) => {
+  const template = db.testimonials[Math.floor(Math.random() * db.testimonials.length)];
+  res.json(template);
+});  
+
 router.route('/testimonials/:id').get ((req, res) => {
   res.json(db.testimonials[`${req.params.id}`-1]);
-});
-  
-router.route('/random').get ((req, res) => {
-  const template = db.testimonials[Math.floor(Math.random() * db.length)];
-  res.json(template);
 });
   
 router.route('/testimonials').post ((req, res) => {
